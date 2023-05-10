17f6fc4458910719da73f6a882d5a1a313137c88
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-10 12:30:49 +0300

    Add using

---
+++ using.rb
@@ -0,0 +1,23 @@
+begin
+    class USING
+        attr_accessor :data1
+        
+        def relative(data1)
+          @tcode =+ "require_relative('#{data1}')"
+          eval(@tcode)
+        end
+        
+        def lib(data1)
+          # TODO: add code here
+        end
+  
+        def ruby(data1)
+          @tcode =+ "require('#{data1}')"
+          eval(@tcode)
+        end
+      end
+      
+
+rescue Exception => e
+    puts "    -error in the std library.\n       #{e.message}".red
+end
