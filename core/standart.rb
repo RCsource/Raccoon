4c32e00ffd86c31f7adb53321935254d99468ab0
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-13 05:35:11 +0300

    add comments to standart.rb

--- core/standart.rb
+++ core/standart.rb
@@ -1,28 +1,33 @@
 begin
-  class STANDART
-    attr_accessor :data
+  class STANDART  # new class
+    attr_accessor :data # var for class
 
-    def bring(data)
-      @tcode =+ "\nputs '#{data}'"
-      eval(@tcode)
-    end
+    def bring(data) # new function
+      @tcode =+ "\nputs #{data}'" # write in public var => "\nputs #{data}'"
+      eval(@tcode) # execute code from public var @tcode
+    end # end function
 
-    def input(data)
+    def input(data) # new funtion
       @tcode =+ "\nprint(data)
       a = STDIN.gets
       return a"
-      eval(@tcode)
-    end
 
-    def wait(data)
-      @tcode =+ "\nsleep 0.#{data}"
-      eval(@tcode)
-    end
+      # write in public var => "
+      # \n print(data)
+      # a = STDIN.gets
+      # return a"
+
+      eval(@tcode) # execute code from public var @tcode
+    end # end function
     
   end
-    def let(data)
+    def let(data) # new function
+      # let
     end
 
 rescue Exception => e
   puts "    -error in the std library.\n       #{e.message}".red
-end
+
+  # if in the std library find erorr write erorr msg
+
+end # end function
