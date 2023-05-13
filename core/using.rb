372386ee5ccfa42bdde91ce8c9a0a0dac9c9a8f1
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-13 05:36:43 +0300

    add comments to using.rb

--- core/using.rb
+++ core/using.rb
@@ -1,23 +1,27 @@
 begin
-    class USING
-        attr_accessor :data1
+    class USING # new class
+        attr_accessor :data1  # var for class
         
-        def relative(data1)
-          @tcode =+ "require_relative('#{data1}')"
-          eval(@tcode)
-        end
+        def relative(data1) # new function
+          @tcode =+ "require_relative('#{data1}')"  # write in public var => "require_relative('#{data1}')"
+          eval(@tcode)  # execute code from public var @tcode
+        end # end function
         
-        def lib(data1)
+        def lib(data1)  # new function
           # TODO: add code here
-        end
+        end # end function
   
-        def ruby(data1)
-          @tcode =+ "require('#{data1}')"
-          eval(@tcode)
-        end
-      end
+        def ruby(data1) # new function
+          @tcode =+ "require('#{data1}')" # write in public var => "require('#{data1})"
+          eval(@tcode)  # execute code from public var @tcode
+        end # end function
+      end # end function
       
 
 rescue Exception => e
-    puts "    -error in the std library.\n       #{e.message}".red
-end
+
+    # if in the using class find
+    # erorr then output errorr msg
+
+    puts "    -error in the using.\n       #{e.message}".red
+end # end function
