4c10350bc249e24333337ee42ae403459d680473
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-13 21:36:12 +0300

    Update repeat.rb

--- core/repeat.rb
+++ core/repeat.rb
@@ -2,18 +2,18 @@
 #some reason the preprocessor does not accept files with the .rn extension
 
 def repeat(data) # new function
-  let i = 0 # new var
+  let i = 0;# new var
   while i < data  # while i < data then start code
     yield # here
-    i += 1  # var "i" + 1
+    i += 1;  # var "i" + 1
   end # end function
 end # end function
 
 def doubles(data) # new function
-  let i = 0 # new var
+  let i = 0; # new var
 
   while i < data * 2 # while i < data(x2) then start code
     yield # here
-    i += 1 # var "i" + 1
+    i += 1; # var "i" + 1
   end # end function
 end # end function
