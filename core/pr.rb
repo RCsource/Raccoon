e5e740af6639fe6837aaba023bec9619be1e4ff3
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-17 20:54:23 +0300

    Update

--- core/pr.rb
+++ core/pr.rb
@@ -3,4 +3,8 @@ require 'colorize'
 require_relative "using.rb"
 require_relative "standart.rb"
 require_relative "time.rb"
+require_relative "yet.rb"
+require_relative "cond.rb"
+require_relative "func.rb"
+
 require_relative "repeat.rb"
