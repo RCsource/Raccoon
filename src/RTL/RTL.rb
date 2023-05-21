6e449014eb381c852900255a4b68c98f91c7fde3
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-21 17:15:38 +0300

    Update RTL main file

--- src/RTL/RTL.rb
+++ src/RTL/RTL.rb
@@ -1,7 +1,7 @@
 begin
     require_relative 'time.rb'
 rescue Exception => e
-    puts "    -error in the RTL library.\n       #{e.message}".red
+    puts "    -error in the RTL module.\n       #{e.message}".red
   
     # if in the rtl library find erorr write erorr msg
   
