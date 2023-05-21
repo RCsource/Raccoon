a99013de0efafbe06eb1e3b5bd3bc30609d20fbd
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-21 17:09:22 +0300

    Update pr.rb file

--- src/pr.rb
+++ src/pr.rb
@@ -3,5 +3,7 @@ require 'colorize'
 require_relative "RUL/RUL.rb"
 require_relative "RSL/RSL.rb"
 require_relative "RTL/RTL.rb"
+
 require_relative "RCS/RCS.rb"
 require_relative "RFL/RFL.rb" 
+require_relative "RRL/RRL.rb"
